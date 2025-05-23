d28eee71ab58921a80d7b8417abf74146edd22ed
daniap03 <danielaunonpardo@gmail.com>
2025-05-23 17:20:15 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,17 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+  let persona =["Mi madre", "Mi padre", "Mi hermano", "Mi perro"];
+  let accion = [ "estropeó", "rompió","tiró", "se comió"];
+  let que = ["mi ordenador,", "mi pantalla,", "mi telefono,", "mis deberes,"];
+  let cuando = ["mientras trabajaba.", "por no poner el lavavajillas.", "a la piscina.", "por la noche."];
   console.log("Hello Rigo from the console!");
-};
+
+  let personaSeleccionada = persona[Math.floor(Math.random() * persona.length)];
+  let accionSeleccionada = accion[Math.floor(Math.random() * accion.length)];
+  let queSeleccionado = que[Math.floor(Math.random() * que.length)];
+  let cuandoSeleccionado = cuando[Math.floor(Math.random() * cuando.length)];
+  let excusa = `${personaSeleccionada} ${accionSeleccionada} ${queSeleccionado} ${cuandoSeleccionado}`;
+  
+  document.getElementById("excuse").innerHTML = excusa
+}
